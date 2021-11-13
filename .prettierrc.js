390f0ed624d93969151772f53f82e33fdfd06cdc
Fabiano Parente <fabiano@nunes.me>
2021-11-12 23:31:22 -0300

    inicial

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+	bracketSpacing: true,
+	bracketSameLine: true,
+	singleQuote: true,
+	semi: false,
+	arrowParens: 'avoid',
+	trailingComma: 'all',
+	importOrder: ['^@[^/](.*)$', '^@/(.*)$'],
+	importOrderSeparation: true,
+	importOrderSortSpecifiers: true,
+	importOrderCaseInsensitive: true,
+	importOrderParserPlugins: ['jsx'],
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# prettier-config
+
+## Instalação
+
+```console
+npm install prettier-config-webcoti --save-dev
+```
+
+## Uso
+
+Adicione ao `package.json`:
+
+```json
+{
+  "prettier": "prettier-config-webcoti"
+}
+```
+
+## [Changelog](CHANGELOG.md)
